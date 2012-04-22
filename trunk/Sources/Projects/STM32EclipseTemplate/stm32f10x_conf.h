a2a0c0be0e78501327f6a7de91a6c91434bc2f8d
pa.eeapai@gmail.com <pa.eeapai@gmail.com@25fb0b70-9576-d08e-36dd-106cf8a58b87>
2012-04-22 10:31:31 +0000

    Added STM32 Eclipse template.

---
+++ trunk/Sources/Projects/STM32EclipseTemplate/stm32f10x_conf.h
@@ -0,0 +1,63 @@
+/////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2012, pa.eeapai@gmail.com                                         //
+// All rights reserved.                                                            //
+//                                                                                 //
+// Redistribution and use in source and binary forms, with or without              //
+// modification, are permitted provided that the following conditions are met:     //
+//     * Redistributions of source code must retain the above copyright            //
+//       notice, this list of conditions and the following disclaimer.             //
+//     * Redistributions in binary form must reproduce the above copyright         //
+//       notice, this list of conditions and the following disclaimer in the       //
+//       documentation and/or other materials provided with the distribution.      //
+//     * Neither the name of the pa.eeapai@gmail.com nor the                       //
+//       names of its contributors may be used to endorse or promote products      //
+//       derived from this software without specific prior written permission.     //
+//                                                                                 //
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND //
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED   //
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE          //
+// DISCLAIMED. IN NO EVENT SHALL pa.eeapai@gmail.com BE LIABLE FOR ANY             //
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES      //
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;    //
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND     //
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT      //
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS   //
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                    //
+/////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __STM32F10x_CONF_H__
+#define __STM32F10x_CONF_H__
+
+//#include "stm32f10x_adc.h"
+//#include "stm32f10x_bkp.h"
+//#include "stm32f10x_can.h"
+//#include "stm32f10x_cec.h"
+//#include "stm32f10x_crc.h"
+//#include "stm32f10x_dac.h"
+//#include "stm32f10x_dbgmcu.h"
+//#include "stm32f10x_dma.h"
+//#include "stm32f10x_exti.h"
+//#include "stm32f10x_flash.h"
+//#include "stm32f10x_fsmc.h"
+#include "stm32f10x_gpio.h"
+//#include "stm32f10x_i2c.h"
+//#include "stm32f10x_iwdg.h"
+//#include "stm32f10x_pwr.h"
+#include "stm32f10x_rcc.h"
+//#include "stm32f10x_rtc.h"
+//#include "stm32f10x_sdio.h"
+//#include "stm32f10x_spi.h"
+//#include "stm32f10x_tim.h"
+//#include "stm32f10x_usart.h"
+//#include "stm32f10x_wwdg.h"
+#include "misc.h"
+
+#ifdef _DEBUG
+  #define ASSERT(e) { (e) ? asm(" NOP") : asm(" BKPT"); }
+#else
+  #define ASSERT(e) { }
+#endif
+
+#define assert_param(expr) ASSERT(expr)
+
+#endif  //  __STM32F10x_CONF_H__
