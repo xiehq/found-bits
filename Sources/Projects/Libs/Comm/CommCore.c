818293dbd304617ebc4542f95bf066be0fb9f182
pa.eeapai <pa.eeapai@gmail.com>
2014-04-09 17:57:49 +0000

    Added CommCore

---
+++ Sources/Projects/Libs/Comm/CommCore.c
@@ -0,0 +1,243 @@
+/////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2014, pa.eeapai@gmail.com                                         //
+// All rights reserved.                                                            //
+//                                                                                 //
+// Redistribution and use in source and binary forms, with or without              //
+// modification, are permitted provided that the following conditions are met:     //
+//     * Redistributions of source code must retain the above copyright            //
+//       notice, this list of conditions and the following disclaimer.             //
+//     * Redistributions in binary form must reproduce the above copyright         //
+//       notice, this list of conditions and the following disclaimer in the       //
+//       documentation and/or other materials provided with the distribution.      //
+//     * Neither the name of the pa.eeapai@gmail.com nor the                       //
+//       names of its contributors may be used to endorse or promote products      //
+//       derived from this software without specific prior written permission.     //
+//                                                                                 //
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND //
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED   //
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE          //
+// DISCLAIMED. IN NO EVENT SHALL pa.eeapai@gmail.com BE LIABLE FOR ANY             //
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES      //
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;    //
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND     //
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT      //
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS   //
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                    //
+/////////////////////////////////////////////////////////////////////////////////////
+
+#include "stdafx.h"
+#include "CommCore.h"
+
+void Comm_PacketStart(SCommLayer *psCommLayer)
+{
+  if ( COMM_LOWER() )
+  { Comm_PacketStart(COMM_LOWER()); }
+  
+  if ( psCommLayer->m_piCommLayer->m_pfnPacketStartAfterLower )
+  { psCommLayer->m_piCommLayer->m_pfnPacketStartAfterLower(psCommLayer); }
+}
+
+void Comm_Send(SCommLayer *psCommLayer, const unsigned char *pbyData, COMMCOUNT cntByteCount)
+{
+  if ( 0 == cntByteCount )
+  { return; }
+  if ( psCommLayer->m_piCommLayer->m_pfnSend )
+  { psCommLayer->m_piCommLayer->m_pfnSend(psCommLayer, pbyData, cntByteCount); }
+  else if ( COMM_LOWER() )
+  { Comm_Send(COMM_LOWER(), pbyData, cntByteCount); }
+}
+
+void Comm_PacketEnd(SCommLayer *psCommLayer)
+{
+  if ( psCommLayer->m_piCommLayer->m_pfnPacketEndBeforeLower )
+  { psCommLayer->m_piCommLayer->m_pfnPacketEndBeforeLower(psCommLayer); }
+
+  if ( COMM_LOWER() )
+  { Comm_PacketEnd(COMM_LOWER()); }
+}
+
+
+void Comm_OnNewPacket(SCommLayer *psCommLayer)
+{
+  if ( psCommLayer->m_piCommLayer->m_pfnOnNewPacketBeforeUpper )
+  { psCommLayer->m_piCommLayer->m_pfnOnNewPacketBeforeUpper(psCommLayer); }
+
+  if ( COMM_UPPER() )
+  { Comm_OnNewPacket(COMM_UPPER()); }
+}
+
+void Comm_OnData(SCommLayer *psCommLayer, const unsigned char *pbyData, COMMCOUNT cntByteCount)
+{
+  if ( 0 == cntByteCount )
+  { return; }
+  if ( psCommLayer->m_piCommLayer->m_pfnOnData )
+  { psCommLayer->m_piCommLayer->m_pfnOnData(psCommLayer, pbyData, cntByteCount); }
+  else if ( COMM_UPPER() )
+  { Comm_OnData(COMM_UPPER(), pbyData, cntByteCount); }
+  else if ( psCommLayer->m_piCommLayer->m_pfnStore )
+  { psCommLayer->m_piCommLayer->m_pfnStore(psCommLayer, pbyData, cntByteCount); }
+}
+
+void Comm_Store(SCommLayer *psCommLayer, const unsigned char *pbyData, COMMCOUNT cntByteCount)
+{
+  if ( 0 == cntByteCount )
+  { return; }
+  if ( psCommLayer->m_piCommLayer->m_pfnStore )
+  { psCommLayer->m_piCommLayer->m_pfnStore(psCommLayer, pbyData, cntByteCount); }
+  else if ( COMM_LOWER() )
+  { Comm_Store(COMM_LOWER(), pbyData, cntByteCount); }
+}
+
+void Comm_OnPacketEnd(SCommLayer *psCommLayer)
+{
+  if ( psCommLayer->m_piCommLayer->m_pfnOnPacketEndBeforeUpper )
+  { psCommLayer->m_piCommLayer->m_pfnOnPacketEndBeforeUpper(psCommLayer); }
+
+  if ( COMM_UPPER() )
+  { Comm_OnPacketEnd(COMM_UPPER()); }
+}
+
+ECommStatus Comm_TransmitProcess(SCommLayer *psCommLayer, COMMCOUNT *pcntNumBytes)
+{
+  unsigned char byStatus = commstatusIdle;
+  *pcntNumBytes = 0;
+  if ( psCommLayer->m_piCommLayer->m_pfnTransmitProcess)
+  {
+    byStatus = psCommLayer->m_piCommLayer->m_pfnTransmitProcess(psCommLayer, pcntNumBytes);
+  }
+  if ( commstatusError == byStatus )
+  {
+    return commstatusError;
+  }
+  if ( COMM_LOWER() )
+  {
+    byStatus = Comm_TransmitProcess(COMM_LOWER(), pcntNumBytes);
+  }
+  return byStatus;
+}
+
+ECommStatus Comm_ReceiveProcess(SCommLayer *psCommLayer, COMMCOUNT *pcntNumBytes)
+{
+  unsigned char byStatus = commstatusIdle;
+  *pcntNumBytes = 0;
+  if ( COMM_LOWER() )
+  {
+    byStatus = Comm_ReceiveProcess(COMM_LOWER(), pcntNumBytes);
+  }
+  if ( commstatusError == byStatus )
+  {
+    return commstatusError;
+  }
+  if ( psCommLayer->m_piCommLayer->m_pfnReceiveProcess)
+  {
+    byStatus = psCommLayer->m_piCommLayer->m_pfnReceiveProcess(psCommLayer, pcntNumBytes);
+  }
+  return byStatus;
+}
+
+COMMCOUNT Comm_GetBufferSize(SCommLayer *psCommLayer)
+{
+  COMMCOUNT cntBufferSize = 0;
+  if ( COMM_LOWER() )
+  { cntBufferSize = Comm_GetBufferSize(COMM_LOWER()); }
+  
+  if ( psCommLayer->m_piCommLayer->m_pfnCommGetBufferSize )
+  { cntBufferSize = psCommLayer->m_piCommLayer->m_pfnCommGetBufferSize(psCommLayer, cntBufferSize); }
+  return cntBufferSize;
+}
+
+void Comm_Disconnect(SCommLayer *psCommLayer)
+{
+  if ( psCommLayer->m_piCommLayer->m_pfnDisconnectBeforeLower )
+  { psCommLayer->m_piCommLayer->m_pfnDisconnectBeforeLower(psCommLayer); }
+
+  if ( COMM_LOWER() )
+  { Comm_Disconnect(COMM_LOWER()); }
+}
+
+HCOMMSTACK CommStack_Init(unsigned char byFlags, SCommStack * psCommStack, SCommLayer *psCommLayers, COMMCOUNT cntLayerCount)
+{
+  COMMCOUNT cntLayerIndex;
+  SCommLayer *psCurrentLayer = (SCommLayer *)NULL;
+  pfnCommEvent pfnInit = COMM_EVENT_NULL;
+  HCOMMSTACK hComm = psCommStack;
+
+  hComm->m_cntNumberOfLayers = cntLayerCount;
+  hComm->m_byFlags = byFlags;
+
+  for ( cntLayerIndex = 0; cntLayerIndex < cntLayerCount; cntLayerIndex++ )
+  {
+    psCommLayers[cntLayerIndex].m_hCommStack = hComm;
+    psCommLayers[cntLayerIndex].m_psLowerLayer = psCurrentLayer;
+    psCommLayers[cntLayerIndex].m_psUpperLayer = &psCommLayers[cntLayerIndex + 1];    
+    psCurrentLayer = &psCommLayers[cntLayerIndex];
+  }
+
+  psCurrentLayer->m_psUpperLayer = (SCommLayer *)NULL;
+  hComm->m_psCommLayer = psCurrentLayer;
+
+  for ( cntLayerIndex = 0; cntLayerIndex < cntLayerCount; cntLayerIndex++ )
+  {
+    if ( commstackflSideClient == ( byFlags & commstackflSideMask ) )
+    {
+      pfnInit = psCurrentLayer->m_piCommLayer->m_pfnClientInit;
+    }
+    else
+    {
+      pfnInit = psCurrentLayer->m_piCommLayer->m_pfnHostInit;
+    }
+    if ( pfnInit )
+    {
+      pfnInit(psCurrentLayer);
+    }
+  }
+
+  return hComm;
+}
+
+
+
+void CommStack_PacketStart(HCOMMSTACK hCommStack)
+{
+  Comm_PacketStart(hCommStack->m_psCommLayer);
+}
+
+void CommStack_Send(HCOMMSTACK hCommStack, const unsigned char *pbyData, COMMCOUNT cntByteCount)
+{
+  Comm_Send(hCommStack->m_psCommLayer, pbyData, cntByteCount);
+}
+void CommStack_PacketEnd(HCOMMSTACK hCommStack)
+{
+  Comm_PacketEnd(hCommStack->m_psCommLayer);
+}
+
+void CommStack_Disconnect(HCOMMSTACK hCommStack)
+{
+  Comm_Disconnect(hCommStack->m_psCommLayer);
+}
+
+ECommStatus CommStack_GetStatus(HCOMMSTACK hCommStack, COMMCOUNT *pcntNumBytes, const unsigned char **ppbyPacketData)
+{
+  ECommStatus eStatus = commstatusActive;
+  Comm_GetStatus(hCommStack->m_psCommLayer, &eStatus, pcntNumBytes, ppbyPacketData);
+  return eStatus;
+}
+
+ECommStatus CommStack_TransmitProcess(HCOMMSTACK hCommStack, COMMCOUNT *pcntNumBytes)
+{
+  BYTE byStatus = Comm_TransmitProcess(hCommStack->m_psCommLayer, pcntNumBytes);
+  return byStatus;
+}
+
+ECommStatus CommStack_ReceiveProcess(HCOMMSTACK hCommStack, COMMCOUNT *pcntNumBytes)
+{
+  BYTE byStatus = Comm_ReceiveProcess(hCommStack->m_psCommLayer, pcntNumBytes);
+  return byStatus;
+}
+
+
+COMMCOUNT CommStack_GetBufferSize(HCOMMSTACK hCommStack)
+{
+  return Comm_GetBufferSize(hCommStack->m_psCommLayer);
+}
+
---
+++ Sources/Projects/Libs/Comm/CommCore.h
@@ -0,0 +1,170 @@
+/////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2014, pa.eeapai@gmail.com                                         //
+// All rights reserved.                                                            //
+//                                                                                 //
+// Redistribution and use in source and binary forms, with or without              //
+// modification, are permitted provided that the following conditions are met:     //
+//     * Redistributions of source code must retain the above copyright            //
+//       notice, this list of conditions and the following disclaimer.             //
+//     * Redistributions in binary form must reproduce the above copyright         //
+//       notice, this list of conditions and the following disclaimer in the       //
+//       documentation and/or other materials provided with the distribution.      //
+//     * Neither the name of the pa.eeapai@gmail.com nor the                       //
+//       names of its contributors may be used to endorse or promote products      //
+//       derived from this software without specific prior written permission.     //
+//                                                                                 //
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND //
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED   //
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE          //
+// DISCLAIMED. IN NO EVENT SHALL pa.eeapai@gmail.com BE LIABLE FOR ANY             //
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES      //
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;    //
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND     //
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT      //
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS   //
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                    //
+/////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __COMM_H__
+#define __COMM_H__
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+struct _SCommLayer;
+typedef struct _SCommLayer SCommLayer;
+
+struct _SCommStack;
+typedef struct _SCommStack SCommStack;
+
+#ifndef COMMCOUNT
+typedef unsigned short COMMCOUNT;
+#endif
+#ifndef COMMINDEX
+typedef unsigned short COMMINDEX;
+#endif
+
+typedef enum _ECommStatus
+{
+  commstatusIdle,     ///< No communication
+  commstatusActive,   ///< Communication is ongoing
+  commstatusNewPacket,///< New packet received
+  commstatusError,    ///< Internal error
+  commstatusOverrun,  ///< Trying to send or receive too much data
+}ECommStatus;
+
+typedef void (*pfnCommEvent)(SCommLayer *psCommLayer);  ///< Layer event notification (init, packet start/end, disconnect etc.)
+typedef void (*pfnCommTransfer)(SCommLayer *psCommLayer, const unsigned char *pbyData, COMMCOUNT cntByteCount); ///< Data tranfer (data received, store, send)
+typedef ECommStatus (*pfnCommProcess)(SCommLayer *psCommLayer, COMMCOUNT *pcntByteCount); ///< Comm process (receive, transmit)
+
+typedef COMMCOUNT (*pfnCommGetBufferSize)(SCommLayer *psCommLayer, COMMCOUNT cntLowerLayerBufferSize);
+
+typedef struct _ICommLayer
+{
+  pfnCommEvent m_pfnClientInit;                 ///< Client mode layer initialization and connect. Communication allowed.
+  pfnCommEvent m_pfnHostInit;                   ///< Host mode layer initialization and start listening.
+
+  pfnCommGetBufferSize m_pfnCommGetBufferSize;  ///< Called from application or upper layer to get max comm buffer length in bytes.
+                                                ///< First same call to lower layer is made.
+                                                ///< Then size from lower layers is available in this call where it can be adjusted and returned.
+
+  pfnCommEvent m_pfnPacketStart;                ///< Called from application or upper layer on new packet transmission after same call to lower layer.
+                                                ///< Same call to lower layer is made by framework regardless if layer implements this call.
+  pfnCommTransfer m_pfnSend;                    ///< Called from application or upper layer to send data.
+                                                ///< Nothing should be sent on physical layer until PacketEnd call.
+                                                ///< If layer implements this call it must also make this call to lower layer.
+                                                ///< Layer can optionaly modify, append, encode etc. data here before forwarding call to lower layer.
+                                                ///< In lowest layer the data is stored in communication buffer as it shold be sent on PacketEnd.
+  pfnCommEvent m_pfnPacketEnd;                  ///< Called from application or upper layer after all data for current packet was provided and before same call to lower layer.
+                                                ///< Same call to lower layer is made by framework regardless if layer implements this call.
+
+  pfnCommProcess m_pfnTransmitProcess;          ///< Repeatedly called from application or upper layer to determine when and if packet is succesfully sent.
+                                                ///< Layer should do transmit work in this call. This shouldn't be a blocking call.
+                                                ///< Return count holds sent byte count until this call.
+                                                ///< Total bytes send in return count can differ from actual packet size.
+
+  pfnCommProcess m_pfnReceiveProcess;           ///< Repeatedly called from application or upper layer to determine if new packet was received.
+                                                ///< Layer should do receiving work in this call. It must call OnNewPacket, OnData and OnPacketEnd.
+                                                ///< This shouldn't be a blocking call.
+                                                ///< Return count holds the byte count of data stored in receive buffer until this call.
+
+  pfnCommEvent m_pfnOnNewPacket;                ///< Called from lower layer when the start of new packet received before same call to upper layer.
+                                                ///< Same call to upper layer is made by framework regardless if layer implements this call.
+  pfnCommTransfer m_pfnOnData;                  ///< Called from lower layer when new data received.
+                                                ///< If layer implements this call it must also make this call to upper layer.
+                                                ///< Layer can optionaly modify, strip, decode etc. data here before forwarding call to upper layer.
+                                                ///< If top most layer doesn't implement this call Store is called on stack.
+  pfnCommTransfer m_pfnStore;                   ///< OnData callback to lower layer to store the data in receive buffer.
+                                                ///< If layer implements this call it must also make this call to lower layer.
+  pfnCommEvent m_pfnOnPacketEnd;                ///< Called from lower layer when complete packet received before same call to upper level.
+                                                ///< Same call to upper layer is made by framework regardless if layer implements this call.
+  
+  pfnCommEvent m_pfnDisconnect;                 ///< Called from application or upper layer to disconnect before same call to lower level.
+                                                ///< Same call to lower layer is made by framework regardless if layer implements this call.
+}ICommLayer;
+
+
+#define COMM_EVENT_NULL         (pfnCommEvent)NULL
+#define COMM_TRANSFER_NULL      (pfnCommTransfer)NULL
+#define COMM_PROCESS_NULL       (pfnCommProcess)NULL
+#define COMM_GETBUFFERSIZE_NULL (pfnCommGetBufferSize)NULL
+
+
+struct _SCommLayer
+{
+  void *m_pLayerInstance;                     ///< "THIS" pointer.
+  ICommLayer *m_piCommLayer;                  ///< Pointer to comm interface implementation.
+  SCommLayer *m_psUpperLayer;                 ///< Pointer to upper comm layer.
+  SCommLayer *m_psLowerLayer;                 ///< Pointer to lower comm layer.
+  HCOMMSTACK m_hCommStack;                    ///< Comm stack handle to which layer belongs.
+};
+
+enum ECommStackFlags
+{
+  commstackflSideClient = 0x00, ///< Stack on client side.
+  commstackflSideHost   = 0x01, ///< Stack on host side.
+  commstackflSideMask   = 0x01,
+};
+
+struct _SCommStack
+{
+  SCommLayer *m_psCommLayer;      ///< Pointer to array of comm layers. Lowest layer (physical) at index 0.
+  COMMCOUNT m_cntNumberOfLayers;  ///< Number of layers in array.
+  BYTE m_byFlags;                 ///< Of ECommStackFlags.
+};
+
+typedef SCommStack * HCOMMSTACK;
+
+void Comm_PacketStart(SCommLayer *psCommLayer);
+void Comm_Send(SCommLayer *psCommLayer, const unsigned char *pbyData, COMMCOUNT cntByteCount);
+void Comm_SendBYTE(SCommLayer *psCommLayer, unsigned char byData);
+void Comm_PacketEnd(SCommLayer *psCommLayer);
+
+void Comm_OnNewPacket(SCommLayer *psCommLayer);
+void Comm_OnData(SCommLayer *psCommLayer, const unsigned char *pbyData, COMMCOUNT cntByteCount);
+void Comm_Store(SCommLayer *psCommLayer, const unsigned char *pbyData, COMMCOUNT cntByteCount);
+void Comm_OnPacketEnd(SCommLayer *psCommLayer);
+COMMCOUNT Comm_GetBufferSize(SCommLayer *psCommLayer);
+
+HCOMMSTACK CommStack_Init(unsigned char byFlags, SCommStack * psCommStack, SCommLayer *psCommLayers, COMMCOUNT cntLayerCount);
+void CommStack_PacketStart(HCOMMSTACK hCommStack);
+void CommStack_Send(HCOMMSTACK hCommStack, const unsigned char *pbyData, COMMCOUNT cntByteCount);
+void CommStack_PacketEnd(HCOMMSTACK hCommStack);
+void CommStack_SendPacket(HCOMMSTACK hCommStack, const unsigned char *pbyData, COMMCOUNT cntByteCount);
+ECommStatus CommStack_TransmitProcess(HCOMMSTACK hCommStack, COMMCOUNT *pcntNumBytes);
+ECommStatus CommStack_ReceiveProcess(HCOMMSTACK hCommStack, COMMCOUNT *pcntNumBytes);
+COMMCOUNT CommStack_GetBufferSize(HCOMMSTACK hCommStack);
+void CommStack_Disconnect(HCOMMSTACK hCommStack);
+
+#define COMM_THIS() psCommLayer->m_pLayerInstance
+#define COMM_UPPER() psCommLayer->m_psUpperLayer
+#define COMM_LOWER() psCommLayer->m_psLowerLayer
+#define COMM_ONDATA(pbyData, cntByteCount) { if ( COMM_UPPER() ) { Comm_OnData(COMM_UPPER(), pbyData, cntByteCount); } else { Comm_Store(psCommLayer, pbyData, cntByteCount); } }
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif  //  __COMM_H__
